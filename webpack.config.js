99cf2df82c55fa4c511c1fd5ae84198d5512c9ed
Wojciech Surmacz <9110562+surmacz@users.noreply.github.com>
2023-02-06 17:05:03 +0100

    Configure webpack & babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Donation Form'
+      template: 'public/index.html',
     }),
   ],
   mode: "development",
